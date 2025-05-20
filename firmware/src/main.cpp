467e6c935df2bf92362f5e7cb3482749b6ef1c14
apache <apache@tencent.com>
2025-05-20 21:00:26 +0800

    add DisplayController

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,56 +1,23 @@
 #include <Arduino.h>
-#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
-// Clockface
 #include <Clockface.h>
-// Commons
 #include <WiFiController.h>
 #include <CWDateTime.h>
 #include <CWPreferences.h>
 #include <CWWebServer.h>
 #include <StatusController.h>
 #include <AudioHelper.h>
+#include <DisplayController.h>
 
 #define ESP32_LED_BUILTIN 2
 #define USER_BUTTON_PIN 36
 #define BUTTON_DEBOUNCE_DELAY 200
 
-MatrixPanel_I2S_DMA *dma_display = nullptr;
-
 Clockface *clockface;
-
 WiFiController wifi;
-//CWDateTime cwDateTime;
-
 volatile bool buttonPressed = false;
 volatile unsigned long lastDebounceTime = 0;
 
-
-void displaySetup(bool swapBlueGreen, uint8_t displayBright, uint8_t displayRotation)
-{
-  HUB75_I2S_CFG mxconfig(64, 64, 1);
-
-  if (swapBlueGreen)
-  {
-    // Swap Blue and Green pins because the panel is RBG instead of RGB.
-    mxconfig.gpio.b1 = 26;
-    mxconfig.gpio.b2 = 12;
-    mxconfig.gpio.g1 = 27;
-    mxconfig.gpio.g2 = 13;
-  }
-
-  mxconfig.gpio.e = 18;
-  mxconfig.clkphase = false;
-
-  // Display Setup
-  dma_display = new MatrixPanel_I2S_DMA(mxconfig);
-  dma_display->begin();
-  dma_display->setBrightness8(displayBright);
-  dma_display->clearScreen();
-  dma_display->setRotation(displayRotation);
-}
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   pinMode(ESP32_LED_BUILTIN, OUTPUT);
   pinMode(USER_BUTTON_PIN, INPUT);
@@ -62,15 +29,14 @@ void setup()
 
   pinMode(ClockwiseParams::getInstance()->ldrPin, INPUT);
 
-  displaySetup(ClockwiseParams::getInstance()->swapBlueGreen, ClockwiseParams::getInstance()->displayBright, ClockwiseParams::getInstance()->displayRotation);
-  clockface = new Clockface(dma_display);
+  DisplayController::getInstance()->begin();
+  clockface = new Clockface(DisplayController::getInstance()->getDmaDisplay());
 
   StatusController::getInstance()->clockwiseLogo();
   delay(1000);
 
   StatusController::getInstance()->wifiConnecting();
-  if (wifi.begin())
-  {
+  if (wifi.begin()) {
     StatusController::getInstance()->ntpConnecting();
     CWDateTime::getInstance()->begin(ClockwiseParams::getInstance()->timeZone.c_str(), 
         ClockwiseParams::getInstance()->use24hFormat, 
@@ -83,19 +49,6 @@ void setup()
     }
   }
 }
-
-void onSerialControl() {
-  int n = Serial.available();
-  if(n > 0) {
-    char cmd[32] = {0};
-    size_t size = Serial.readBytes(cmd, n);
-    cmd[size - 1] = '\0';
-    if(strcmp(cmd, "jump") == 0) {
-      clockface->externalEvent(0);
-    }
-  }
-}
-
 
 void onButtonEvent() {
   bool value = digitalRead(USER_BUTTON_PIN);
@@ -115,22 +68,16 @@ void onButtonEvent() {
   }
 }
 
-void loop()
-{
-  wifi.handleImprovWiFi();
-
+void loop() {
   /*
   if (wifi.isConnected())
   {
     ClockwiseWebServer::getInstance()->handleHttpRequest();
   }
   */
-
-  if (wifi.connectionSucessfulOnce)
-  {
+  wifi.handleImprovWiFi();
+  if(wifi.connectionSucessfulOnce) {
     clockface->loop();
   }
-
-  onSerialControl();
   onButtonEvent();
 }
