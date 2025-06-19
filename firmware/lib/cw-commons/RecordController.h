69430e1722c09d05165dd87bc1043e55d1fa5bf5
apache(何庆玮) <apache@tencent.com>
2025-06-19 17:39:36 +0800

    use websocket for recording data upload

--- firmware/lib/cw-commons/RecordController.h
+++ firmware/lib/cw-commons/RecordController.h
@@ -1,14 +1,13 @@
 #pragma once
 #include <Arduino.h>
-#include <CWPreferences.h>
-#include <SPIFFS.h>
 #include <driver/i2s.h>
-#include "ChatController.h"
+#include <WebSocketsClient.h>
 
-#define WAV_FILE "/record3.wav"
-#define WAV_HEADER_SIZE   44
-
+extern WebSocketsClient webSocket;
 volatile bool recording = false;
+const uint8_t RECORD_BEGIN_BUFF[] = {'\xBB', '\xBB'};
+const uint8_t RECORD_END_BUFF[] = {'\xEE', '\xEE'};
+
 
 struct RecordController {
   static RecordController *getInstance() {
@@ -23,85 +22,22 @@ struct RecordController {
   bool isRecording() {
     return recording;
   }
+
   
-  static void buildWavHeader(uint8_t* header, int wavSize){
-    header[0] = 'R';
-    header[1] = 'I';
-    header[2] = 'F';
-    header[3] = 'F';
-    unsigned int fileSize = wavSize + WAV_HEADER_SIZE - 8;
-    header[4] = (uint8_t)(fileSize & 0xFF);
-    header[5] = (uint8_t)((fileSize >> 8) & 0xFF);
-    header[6] = (uint8_t)((fileSize >> 16) & 0xFF);
-    header[7] = (uint8_t)((fileSize >> 24) & 0xFF);
-    header[8] = 'W';
-    header[9] = 'A';
-    header[10] = 'V';
-    header[11] = 'E';
-    header[12] = 'f';
-    header[13] = 'm';
-    header[14] = 't';
-    header[15] = ' ';
-    header[16] = 0x10; // linear PCM
-    header[17] = 0x00;
-    header[18] = 0x00;
-    header[19] = 0x00;
-    header[20] = 0x01; // linear PCM
-    header[21] = 0x00;
-    header[22] = 0x01; // monoral
-    header[23] = 0x00;
-    header[24] = 0x80; // sampling rate 16000
-    header[25] = 0x3E;
-    header[26] = 0x00;
-    header[27] = 0x00;
-    header[28] = 0x00;
-    header[29] = 0x7D;
-    header[30] = 0x00; //0x01
-    header[31] = 0x00;
-    header[32] = 0x02; // 16bit monoral
-    header[33] = 0x00;
-    header[34] = 0x10; // 16bit
-    header[35] = 0x00;
-    header[36] = 'd';
-    header[37] = 'a';
-    header[38] = 't';
-    header[39] = 'a';
-    header[40] = (uint8_t)(wavSize & 0xFF);
-    header[41] = (uint8_t)((wavSize >> 8) & 0xFF);
-    header[42] = (uint8_t)((wavSize >> 16) & 0xFF);
-    header[43] = (uint8_t)((wavSize >> 24) & 0xFF);
-  }
-
-
-  static void dataScale(uint8_t * d_buff, uint8_t* s_buff, uint32_t len) {
-    uint32_t j = 0;
-    uint32_t dac_value = 0;
-    for (int i = 0; i < len; i += 2) {
-      dac_value = ((((uint16_t) (s_buff[i + 1] & 0xf) << 8) | ((s_buff[i + 0]))));
-      d_buff[j++] = 0;
-      d_buff[j++] = dac_value * 256 / 4096;
-    }
-  }
-
   static void recordTask(void *param) {
     size_t audioSize = 0;
+    int32_t audioBuffer[DMA_BUF_LEN] = {0};
     int16_t pcmBuffer[DMA_BUF_LEN] = {0};
-    int16_t wavBuffer[DMA_BUF_LEN] = {0};
-    
-    SPIFFS.remove(WAV_FILE);
-    File fp = SPIFFS.open(WAV_FILE, FILE_WRITE);
-    if(!fp) {
-      Serial.println("fail to create file");
-      goto end;
-    }
 
     Serial.println("===> recording...");
-    fp.seek(WAV_HEADER_SIZE);    
     I2SController::getInstance()->switchToRx();
     I2SController::getInstance()->switchLock(); // 等待switchToRx切换完成
+
+    webSocket.sendBIN(RECORD_BEGIN_BUFF, sizeof(RECORD_BEGIN_BUFF));
+    
     while(recording) {
       size_t size = 0;
-      esp_err_t ret = i2s_read(I2S_PORT, pcmBuffer, DMA_BUF_LEN / sizeof(pcmBuffer[0]), &size, portMAX_DELAY); // pdMS_TO_TICKS(10)
+      esp_err_t ret = i2s_read(I2S_PORT, audioBuffer, DMA_BUF_LEN / sizeof(audioBuffer[0]), &size, portMAX_DELAY); // pdMS_TO_TICKS(10)
       if (ret != ESP_OK ) {
         if(ret == ESP_ERR_INVALID_STATE) {
           i2s_stop(I2S_PORT);
@@ -113,23 +49,24 @@ struct RecordController {
       } else if(size <= 0) {
         vTaskDelay(1);
       } else {
-        dataScale((uint8_t*)wavBuffer, (uint8_t*)pcmBuffer, size);
-        fp.write((uint8_t *)wavBuffer, size);
+        int samples = size / sizeof(audioBuffer[0]);
+        for(int i = 0; i < samples; i++) {
+          pcmBuffer[i] = (int16_t)(audioBuffer[i] >> 16);
+        }
+        webSocket.sendBIN((uint8_t*)pcmBuffer, samples * sizeof(pcmBuffer[0]));
         audioSize += size;
       }
     }
-    I2SController::getInstance()->switchUnlock();
-    uint8_t header[WAV_HEADER_SIZE];
-    buildWavHeader(header, audioSize);
-    fp.seek(0);
-    fp.write(header, WAV_HEADER_SIZE);
-    fp.close();
-    Serial.printf("record finished, audio size is %d\n", audioSize);
-    ChatController::getInstance()->chatToServer();
-end:
+
     recording = false;
+    
+    I2SController::getInstance()->switchUnlock();
+    Serial.printf("record finished, audio size is %d\n", audioSize);
+    webSocket.sendBIN(RECORD_END_BUFF, sizeof(RECORD_END_BUFF));
+    
     // 录音完成后需要主动切换到音频播放模式
     I2SController::getInstance()->switchToTx();
+    
     vTaskDelete(NULL);
   }
 
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -9,11 +9,19 @@
 #include <DisplayController.h>
 #include <ButtonController.h>
 #include <RecordController.h>
+#include <WebSocketsClient.h>
+
 
 #define ESP32_LED_BUILTIN 2
+#define WEBSOCKET_SERVER  "47.115.38.84"
+#define WEBSOCKET_PORT    8000
+#define WEBSOCKET_AUDIO_PATH "/audio"
+
 
 Clockface *clockface;
 WiFiController wifi;
+WebSocketsClient webSocket;
+
 /*
 volatile bool buttonPressed = false;
 volatile unsigned long lastDebounceTime = 0;
@@ -31,6 +39,33 @@ void onButtonLongPressStart() {
 void onButtonLongPressStop() {
   Serial.println("button long press stop");
   RecordController::getInstance()->stopRecord();
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_PING:
+      break;
+    case WStype_PONG:
+      break;
+    case WStype_DISCONNECTED:
+      Serial.println("[WSc] Disconnected!");
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WSc] Connected to url: %s\n", payload);
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      break;
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+      break;
+  }
 }
 
 void setup() {
@@ -62,6 +97,9 @@ void setup() {
         ClockwiseParams::getInstance()->alarmClock.c_str());
     clockface->init();
     if(wifi.isConnected()) {
+      webSocket.begin(WEBSOCKET_SERVER, WEBSOCKET_PORT, WEBSOCKET_AUDIO_PATH);
+      webSocket.onEvent(webSocketEvent);
+      webSocket.setReconnectInterval(3000);
       ClockwiseWebServer::getInstance()->handleHttpRequestInTask();
     }
   }
@@ -95,6 +133,7 @@ void loop() {
     ClockwiseWebServer::getInstance()->handleHttpRequest();
   }
   */
+  webSocket.loop();
   ButtonController::getInstance()->loop();
   wifi.handleImprovWiFi();
   if(wifi.connectionSucessfulOnce) {
