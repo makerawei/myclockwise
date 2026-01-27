90e76721c93116bfd54827ead151fce5da66e970
apache <apache@tencent.com>
2026-01-27 21:30:07 +0800

    add web page for install firmware online

--- merge.sh
+++ merge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-esptool.py --chip esp32 merge_bin -o myclockwise_merge_esp32.bin --flash_mode dio --flash_freq 40m --flash_size 4MB \
+esptool.py --chip esp32 merge_bin -o web/firmware/myclockwise_merge_esp32.bin --flash_mode dio --flash_freq 40m --flash_size 4MB \
   0x1000  build/bootloader/bootloader.bin \
   0x8000  build/partition_table/partition-table.bin \
   0xe000  build/ota_data_initial.bin \
